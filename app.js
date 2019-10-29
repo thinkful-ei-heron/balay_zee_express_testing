3b05b0e9cf4ee9d98a44b811a7817bde6f5b011a
balayaydemir <balayaydemir@gmail.com>
2019-10-29 08:09:35 -0700

    skeleton set up

---
+++ app.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.get('/apps', (req, res) => {
+    
+});
+
+app.listen(8000, () => {
+  console.log('listening!');
+});
